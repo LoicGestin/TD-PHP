35d6ae8e57a42e06cf6fbfae8d32d8a35578fb9f
LoicGestin <loic.gestin34@gmail.com>
2021-09-21 12:43:24 +0200

    TD3 début

---
+++ TD3/Voiture.php
@@ -0,0 +1,98 @@
+<?php
+   
+class Voiture {
+   
+    private $marque;
+    private $couleur;
+    private $immatriculation;
+
+    public function __construct($m = NULL, $c = NULL, $i = NULL) {
+        if (!is_null($m) && !is_null($c) && !is_null($i)) {
+            // Si aucun de $m, $c et $i sont nuls,
+            // c'est forcement qu'on les a fournis
+            // donc on retombe sur le constructeur à 3 arguments
+            $this->marque = $m;
+            $this->couleur = $c;
+            $this->immatriculation = $i;
+        }
+    }
+    // un getter      
+    public function getMarque() {
+        return $this->marque;
+    }
+
+    public function getCouleur() {
+        return $this->couleur;
+    }
+   
+
+    public function getImmatriculation() {
+        return $this->immatriculation;
+    }
+    public static function getVoitureByImmat($immat) {
+        $sql = "SELECT * from voiture WHERE immatriculation=:nom_tag";
+        // Préparation de la requête
+        $req_prep = Model::getPDO()->prepare($sql);
+
+        $values = array(
+            "nom_tag" => $immat,
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête
+        $req_prep->execute($values);
+
+        // On récupère les résultats comme précédemment
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+        $tab_voit = $req_prep->fetchAll();
+        // Attention, si il n'y a pas de résultats, on renvoie false
+        if (empty($tab_voit))
+            return false;
+        return $tab_voit[0];
+    }
+
+    public static function getAllVoitures(){
+
+       $sql_request = "SELECT * FROM voiture";
+       try {
+           $rep = Model::getPdo()->query($sql_request);
+           $rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+           $tab_voit = $rep->fetchAll();
+       }
+       catch (PDOException $e) {
+           if (Conf::getDebug()) {
+               echo $e->getMessage(); // affiche un message d'erreur
+           } else {
+               echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+           }
+           die();
+       }
+
+       return $tab_voit;
+    }
+    // un setter 
+    public function setMarque($m) {
+        $this->marque = $m;
+    }
+    public function setCouleur($m) {
+        $this->couleur = $m;
+    }
+    public function setImmatriculation($m) {
+        if (strlen($m) <= 8){
+            $this->immatriculation = $m;
+        }
+    }
+   
+    // un constructeur
+    public function __construct2($m, $c, $i) {
+        $this->marque = $m;
+        $this->couleur = $c;
+        $this->immatriculation = $i;
+    } 
+              
+    // une methode d'affichage.
+    public function afficher() {
+        echo " <h5>Marque = $this->marque <br> Immatriculation = $this->immatriculation <br> Couleur = $this->couleur</h5>";
+    }
+}
+?>
+
---
+++ TD3/lireVoiture.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'Model.php';
+require_once '../TD1/Voiture.php';
+require_once '../TD1/Utilisateur.php';
+require_once '../TD1/Trajet.php';
+
+    /*
+    $sql_request = "SELECT * FROM voiture";
+    $rep = Model::getPdo()->query($sql_request);
+    /*
+    $tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
+
+    var_dump($tab_obj);
+    foreach ($tab_obj  as $key => $value) {
+        //print "$key => $value\n";
+        echo "<li> ";
+        foreach ($value as $key => $oui) {
+
+            echo " $oui ";
+
+        }
+        echo "   </li>";
+    }
+
+    $rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+    $tab_voit = $rep->fetchAll();
+    $num = 0;
+    */
+    $tab_voit = Voiture::getAllVoitures();
+    $num = 0;
+    foreach ($tab_voit  as $key => $value) {
+        $num += 1;
+        echo "<h2>Voiture n°$num </h2><br>";
+        $value->afficher();
+        echo "<br>";
+    }
+
+    $tab_voit = Utilisateur::getAllUtilisateurs();
+    $num = 0;
+    foreach ($tab_voit  as $key => $value) {
+        $num += 1;
+        echo "<h2>Utilisateur n°$num </h2><br>";
+        $value->afficher();
+        echo "<br>";
+    }
+
+    $tab_voit = Trajet::getAllTrajets();
+    $num = 0;
+    foreach ($tab_voit  as $key => $value) {
+        $num += 1;
+        echo "<h2>Trajet n°$num </h2><br>";
+        $value->afficher();
+        echo "<br>";
+    }
+
+    $tab_voit = Voiture::getVoitureByImmat("0707068A");
+    foreach ($tab_voit  as $key => $value) {
+        $num += 1;
+        echo "<h2>Trajet n°$num </h2><br>";
+        $value->afficher();
+        echo "<br>";
+    }
+
+
+
+
