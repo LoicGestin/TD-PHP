fc20a0290d26107fab7bd3d74164312f0f0b52c2
LoicGestin <loic.gestin34@gmail.com>
2021-10-12 14:27:56 +0200

    TD6 FIN + bonous

--- TD4/index.php
+++ TD4/index.php
@@ -1,6 +1,4 @@
 <?php
-    require __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'File.php';
-    require 'controller/routeur.php'
 
-
-?>
+require __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'File.php';
+require 'controller/routeur.php';
